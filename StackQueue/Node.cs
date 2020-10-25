bb17cc6a833a9c0c623d1a868eef7a447f58e25d
N1vas <ggj.nivas.18@gmail.com>
2020-10-25 23:07:55 +0530

    [N1vas] Add : Stack has been created

---
+++ StackQueue/Node.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace StackQueue
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ StackQueue/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace StackQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListStack stack = new LinkedListStack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+        }
+    }
+}
